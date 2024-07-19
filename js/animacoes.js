545c079c329c387629a1b4e5dffa6ee3f719e82d
Gabriel Strolego <gsq.25@hotmail.com>
2024-07-19 00:01:41 -0300

    Initial commit

---
+++ js/animacoes.js
@@ -0,0 +1,133 @@
+
+
+// Função de rolagem suave para um ID específico
+function jumpToId(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+// assume o elemento pelo ID
+function inicioID() {
+  jumpToId('inicio');
+}
+
+function quemSomosID() {
+  jumpToId('quemsomos');
+}
+
+function contatosID() {
+  jumpToId('contatos');
+}
+
+// Funções para atualizar as classes dos botões
+function atualizarClasses(botaoAtivo) {
+  const botoes = ['Inicio', 'QuemSomos', 'Contatos'];
+  
+  botoes.forEach(botao => {
+    const elemento = document.getElementById(`btnMenu${botao}`);
+    if (botao === botaoAtivo) {
+      elemento.classList.remove('btn-outline-secondary');
+      elemento.classList.add('btn-success');
+    } else {
+      elemento.classList.remove('btn-success');
+      elemento.classList.add('btn-outline-secondary');
+    }
+  });
+}
+
+function irParaInicio() {
+  inicioID();
+}
+
+function irParaQuemSomos() {
+  quemSomosID();
+}
+
+function irParaContatos() {
+    contatosID();
+}
+
+
+
+// Função para criar um IntersectionObserver
+function criarObserver(elementId, callback) {
+  const elemento = document.getElementById(elementId);
+    
+  if (elemento) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          callback();
+        }
+      });
+    }, {
+      threshold: 0.4 // Ajuste este valor conforme necessário
+    });
+      
+    observer.observe(elemento);
+  }
+}
+
+
+// Configura os observers para cada seção
+function observarInicio() {
+  atualizarClasses('Inicio');
+}
+
+function observarQuemSomos() {
+  atualizarClasses('QuemSomos');
+}
+
+function observarContatos() {
+  atualizarClasses('Contatos');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  criarObserver('inicio',observarInicio);
+  criarObserver('quemsomos', observarQuemSomos);
+  criarObserver('contatos', observarContatos);
+});
+
+const texts = ["Qualidade e Rapidez nas entregas!", "Funcionamos em Horário Comercial.", "Entre em Contato pelo nosso Telefone ou E-mail."];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+let isDeleting = false;
+const cursor = document.getElementById('cursor');
+
+function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+
+    if (isDeleting) {
+        letter = currentText.slice(0, index--);
+    } else {
+        letter = currentText.slice(0, ++index);
+    }
+
+    document.getElementById('text').textContent = letter;
+
+    if (!isDeleting && letter.length === currentText.length) {
+        cursor.classList.add('blink');  // Adiciona a classe blink antes de deletar
+        setTimeout(() => {
+            cursor.classList.remove('blink');  // Remove a classe blink
+            isDeleting = true;
+            setTimeout(type, 100);  // Velocidade de apagamento
+        }, 2000);  // Tempo para esperar antes de começar a apagar a frase
+    } else if (isDeleting && letter.length === 0) {
+        isDeleting = false;
+        count++;
+        cursor.classList.add('blink');  // Adiciona a classe blink antes de começar a próxima frase
+        setTimeout(() => {
+            cursor.classList.remove('blink');  // Remove a classe blink
+            type();
+        }, 700);  // Tempo para começar a digitar a próxima frase
+    } else {
+        setTimeout(type, isDeleting ? 9 : 90);  // Velocidade de digitação e apagamento
+    }
+}
+
+// Inicia a função de digitação
+type();
